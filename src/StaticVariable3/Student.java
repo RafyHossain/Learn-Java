e0125a88f66e12284bc54bfc8e06371401979519
RafyHossain <rafyhossain56@gmail.com>
2025-07-19 11:36:06 +0600

    static variable 3

---
+++ src/StaticVariable3/Student.java
@@ -0,0 +1,13 @@
+package StaticVariable3;
+//count variable e static use na korle student er prottekta object er jonno count=0 initialize hbe
+//statict int count use korle cout++ hoite thakbe
+
+public class Student {
+    static int count=0;
+    Student(){
+        count++;
+    }
+    void display(){
+        System.out.println("Total student: "+count);
+    }
+}
---
+++ src/StaticVariable3/Test.java
@@ -0,0 +1,17 @@
+package StaticVariable3;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        Student s2=new Student();
+        Student s3=new Student();
+        Student s4=new Student();
+        Student s5=new Student();
+        Student s6=new Student();
+        Student s7=new Student();
+        Student s8=new Student();
+        Student s9=new Student();
+        Student s10=new Student();
+        s10.display();
+    }
+}
