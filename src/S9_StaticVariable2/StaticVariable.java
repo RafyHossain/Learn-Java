6a51d22207c6aa7424bd3367ca73d4644f61931d
RafyHossain <rafyhossain56@gmail.com>
2025-07-19 11:11:56 +0600

    static variable 2

---
+++ src/S9_StaticVariable2/StaticVariable.java
@@ -0,0 +1,5 @@
+package S9_StaticVariable2;
+
+public class StaticVariable {
+    static String university="Patuakhali Science and Technology University";
+}
---
+++ src/S9_StaticVariable2/Test.java
@@ -0,0 +1,9 @@
+package S9_StaticVariable2;
+
+public class Test {
+    public static void main(String[] args) {
+        //Static variable er jonno kono object create kora lage na
+        //direct class name diye call korte hoy
+        System.out.println("University name: "+ StaticVariable.university);
+    }
+}
