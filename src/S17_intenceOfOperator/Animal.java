73ed7ae4aadf602de49037df69ccae831458145a
RafyHossain <rafyhossain56@gmail.com>
2025-07-27 21:43:59 +0600

    Intance of

---
+++ src/S17_intenceOfOperator/Animal.java
@@ -0,0 +1,4 @@
+package S17_intenceOfOperator;
+
+public class Animal {
+}
---
+++ src/S17_intenceOfOperator/Test.java
@@ -0,0 +1,15 @@
+package S17_intenceOfOperator;
+
+public class Test {
+    public static void main(String[] args) {
+        Animal animal = new Teacher();
+        Teacher teacher = new Teacher();
+        Student student = new Student();
+
+        System.out.println(teacher instanceof Animal);
+        System.out.println(animal instanceof Animal);
+        System.out.println(student instanceof Animal);
+        System.out.println(student instanceof Teacher);
+
+    }
+}
